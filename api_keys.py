b8de4fce1dcf6f8b1f30bee37de1729078aa178c
seobeeeee1001 <110898877+seobeeeee1001@users.noreply.github.com>
2025-05-12 17:52:40 +0900

    Update api_keys.py

--- api_keys.py
+++ api_keys.py
@@ -4,8 +4,8 @@ API 키 설정 파일
 """
 
 # API 키 설정
-DART_API_KEY = "eca07894492aaef0e5019bd7137330fe685e4875"  # DART API 키 입력 (https://opendart.fss.or.kr/에서 신청)
-KRX_API_KEY = "2DC04B47D4D84A02BD467C2ED0BBAFBA47697670"    # KRX API 키 입력 (http://data.krx.co.kr/에서 신청)
+DART_API_KEY = ""  # DART API 키 입력 (https://opendart.fss.or.kr/에서 신청)
+KRX_API_KEY = ""    # KRX API 키 입력 (http://data.krx.co.kr/에서 신청)
 
 # 기본 설정값
 DEFAULT_TICKER = "005930"  # 삼성전자
